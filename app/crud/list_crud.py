f304e7971562f404a0776e50d0635b588ddfe669
satsuki2200 <132316826+satsuki2200@users.noreply.github.com>
2024-12-10 00:29:09 +0900

    station16 done

--- app/crud/list_crud.py
+++ app/crud/list_crud.py
@@ -11,8 +11,15 @@ def get_todo_lists(db: Session):
     return db_lists
 
 def get_todo_list(db: Session, todo_list_id: int):
-    db_list = db.query(ListModel).filter(ListModel.id == todo_list_id).first()
-    return db_list
+    try:
+        db_list = db.query(ListModel).filter(ListModel.id == todo_list_id).first()
+        if not db_list:
+            raise HTTPException(status_code=404, detail='Todo List Not Found')
+        return db_list
+
+    except Exception as e:
+        db.rollback()
+        raise e
 
 def create_todo_list(db: Session, data: NewTodoList):
     new_db_list = ListModel(
@@ -25,39 +32,31 @@ def create_todo_list(db: Session, data: NewTodoList):
     return new_db_list
 
 def update_todo_list(db: Session, todo_list_id: int, update_todo_list: UpdateTodoList):
-    error = None
     try:
         db_item = db.query(ListModel).filter(ListModel.id == todo_list_id).first()
-        if db_item is None:
+        if not db_item:
             raise HTTPException(status_code=404, detail='Todo List Not Found')
         db_item.title = update_todo_list.title
         db_item.description = update_todo_list.description
         db.commit()
-
-    except Exception as e:
-        error = e
-        db.rollback()
-    
-    finally:
-        if error is not None:
-            return error
         db.refresh(db_item)
         return db_item
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        db.rollback()
+        raise e
     
 async def delete_todo_list(db: Session, todo_list_id: int):
-    status = None
     try:
         db_list = db.query(ListModel).filter(ListModel.id == todo_list_id).first()
-        if db_list is None:
+        if not db_list:
             raise HTTPException(status_code=404, detail='Todo List Not Found')
         db.delete(db_list)
         db.commit()
-        db.refresh(db_list)
-        status = {"status": True}
-
+        return {"status": True}
+    except HTTPException as e:
+        raise e
     except Exception as e:
-        status = e
         db.rollback()
-
-    finally:
-        return HTTPException(status_code=200, detail=status)
+        raise e
